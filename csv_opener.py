de4e956ac8b6c297966eda06ad5598d863cc2210
MichaelAGunn <michael.gunn.mlis@gmail.com>
2019-05-20 15:46:52 -0400

    Update csv_opener.py

--- csv_opener.py
+++ csv_opener.py
@@ -1,37 +1,17 @@
 #This is a basic prototype that simply
 #opens the csv file and creates a dictionary
-from csv import reader
+import csv
 
-opened_file = open('kjv.csv')
-read_file = reader(opened_file)
-Bible = list(read_file)
-Bible = Bible[1:]
+#Convert csv file into a dictionary
+kjv_file = open("kjv.csv")
+kjv_table = csv.reader(kjv_file)
+kjv_table = list(kjv_table)
+kjv_table = kjv_table[1:]
 
-#Data are cleaned (turned into integers)
-def clean_db(list_of_lists):
-	for row in list_of_lists:
-		row[0] = int(row[0])
-		row[1] = int(row[1])
-		row[2] = int(row[2])
-		row[3] = int(row[3])
-
-clean_db(Bible)
-
-#Create the text to display
-def read_book(list_of_lists, b):
-	text_to_display = []
-	for row in list_of_lists:
-		if int(row[1]) == b:
-			text_to_display.append(row[4])
-	return text_to_display
-
-#The actual printing of the text
-book = input("Pick a number between 1 and 66: ")
-book = int(book)
-if book >= 1 and book <= 66:
-	displayed_text = read_book(Bible, book)
-	for row in Bible:
-		if row[1] == book:
-			print(row[4])
-else:
-	print("Invalid Input")
+#Display text and partition by book and chapter.
+for row in kjv_table[0:10]: #substitute any range of rows
+    if row[2] == "1" and row[3] == "1":
+            print("\n\nBook " + row[1] + "\n") #book heading
+    if row[3] == "1":
+            print("\nChapter " + row[2] + "\n") #chapter heading
+    print(row[4])
