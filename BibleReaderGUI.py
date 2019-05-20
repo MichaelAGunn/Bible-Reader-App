6c32860b1733bfa2ca569c77afad2866fd8558b1
Michael Gunn <michael.gunn.mlis@gmail.com>
2019-05-20 09:32:25 -0400

    Update BibleReaderGUI.py

--- BibleReaderGUI.py
+++ BibleReaderGUI.py
@@ -1,32 +1,62 @@
-from tkinter import *
+import tkinter as tk
+import tkinter.filedialog
 
+#Setting attributes for the window
+WIDTH = 1880
+HEIGHT = 1060
+TITLE = "Bible Reader"
 
-root = Tk()
-root.mainloop()
+#Produce the GUI window as a canvas
+root = tk.Tk()
+root.title(TITLE)
+canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
+canvas.pack()
 
-screen_height = 1080
-screen_width = 1920
+#Defining the quit command
+def quit_app(event=None):
+	root.quit()
 
-
-root.title("Bible Reader")
-root.geometry("1920x1080")
-root.config(bg='cadet blue')
-
-'''
 #----1st frame----
 #Frame
-r1_frame = Frame(width=10, height=10).place(x=(int(screen_width / 2)), y=100, padx=10, pady=10)
+frame1 = tk.Frame(root, bg='red')
+frame1.place(relx=0.5, relwidth=1, relheight=0.05, anchor='n')
 #Label
-search_label = Label().place
+search_label = tk.Label(frame1, text="Search here:", bg='red')
+search_label.place(relwidth=0.05, anchor='nw')
 #Entry
+search_entry = tk.Entry(frame1)
+search_entry.place(relx=0.5, relwidth=0.89, anchor='n')
 #Button
+search_button = tk.Button(frame1, text='Submit')
+search_button.place(relx=1, relwidth=0.05, anchor='ne')
 
 #----2nd frame----
 #Frame
-#Label
+frame2 = tk.Frame(root, bg='white')
+frame2.place(relx=0.5, rely=0.05, relwidth=1, relheight=0.9, anchor='n')
+#Text
+display_default = ["This is where", " ",
+				   "the query text", " ",
+				   "will be displayed.", "\n",
+				   "Thus saith the Lord,", " ",
+				   "\"Do not maketh thine", " ",
+				   "work into a spaghetti.\"","\n",
+				   "Amen.", "\n\n\n***\n\n"]
+display_label = tk.Label(frame2, text=display_default, bg='white')
+display_label.place(relwidth=1, relheight=1)#Scrollbar?
 
 #----3rd frame----
 #Frame
-#Labels (how many?)
-'''
+frame3 = tk.Frame(root, bg='red')
+frame3.place(relx=0.5, rely=0.95, relwidth=1, relheight=0.1, anchor='n')
+#Label 1
+version_current = "King James Version"
+version_label = tk.Label(frame3, text=version_current, bg='red')
+version_label.place(relx=0.1)
+#Label 2
+query_current = "Hezekiah 3:16"
+query_label = tk.Label(frame3, text="Last Query: \""+query_current+"\"", bg='red')
+query_label.place(relx=0.2)
 
+#Build the App!
+root.mainloop()
